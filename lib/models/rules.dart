a95143b59a925b1215bf727885c7e86d1fb1b163
ardhikakrsh <ardhikakrishna@gmail.com>
2025-06-08 00:06:13 +0700

    feat: add rules model

---
+++ lib/models/rules.dart
@@ -0,0 +1,11 @@
+class Rules {
+  final String id;
+  final String title;
+  final List<String> content;
+
+  Rules({
+    required this.id,
+    required this.title,
+    required this.content,
+  });
+}
